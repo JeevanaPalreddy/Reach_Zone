9c3810110d50e276d7d33e213dfc34fa123958d9
Laxmi Varshitha J <varshithajuturu2006@gmail.com>
2026-04-15 11:08:35 +0530

    Change API base URL to localhost:5003

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
    const API = axios.create({
-     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5002/api'
+     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5003/api'
 });
    API.interceptors.request.use(req => {
      const token = localStorage.getItem('token');
